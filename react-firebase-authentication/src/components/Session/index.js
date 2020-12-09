6c6a1f52082d57dc57b3e6eb69e4b36fa2e257bb
gullnaz <75530785+gullnaz@users.noreply.github.com>
2020-12-10 00:19:43 +0600

    first commit

---
+++ react-firebase-authentication/src/components/Session/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
+import {withFirebase } from '../Firebase';
+import * as ROUTES from '../../constants/routes';
+import AuthUserContext from './context';
+import withAuthentication from './withAuthentication';
+
+const withAuthorization = condition => Component => {
+  class WithAuthorization extends React.Component {
+    componentDidMount() {
+      this.listener = this.props.firebase.auth.onAuthStateChanged(
+        authUser => {
+          if (!condition(authUser)) {
+            this.props.history.push(ROUTES.SIGN_IN);
+          }
+        },
+      );
+    }
+
+    componentWillUnmount() {
+      this.listener();
+    }
+
+    render() {
+      return (
+        <AuthUserContext.Consumer>
+          {authUser =>
+            condition(authUser) ? <Component {...this.props} /> : null
+          }
+        </AuthUserContext.Consumer>
+      );
+    }
+  }
+
+  return compose(
+    withRouter,
+    withFirebase,
+  )(WithAuthorization);
+};
+
+export { AuthUserContext, withAuthentication, withAuthorization };
+
+export default withAuthorization;
